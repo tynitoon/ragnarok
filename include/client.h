b5f010b3d7f6f5adb925462d4112e28d66f9bdd6
jeremie mortier <mortier.jeremie@gmail.com>
2023-02-11 11:39:47 +0100

    start cross platform for tools

--- include/client.h
+++ include/client.h
@@ -1,18 +1,18 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include "list.h"
-
-#define BUFFER_SIZE 4096
-
-typedef struct  s_server
-{
-    int         fd;
-    t_list      messages;
-    size_t      buffer_index;
-    char        buffer[BUFFER_SIZE];
-}               t_server;
-
-int start_client(char* address, int port, t_server* server);
+//#include "list.h"
+//
+//#define BUFFER_SIZE 4096
+//
+//typedef struct  s_server
+//{
+//    int         fd;
+//    t_list      messages;
+//    size_t      buffer_index;
+//    char        buffer[BUFFER_SIZE];
+//}               t_server;
+//
+//int start_client(char* address, int port, t_server* server);
 
 #endif
--- include/single_memory.h
+++ include/single_memory.h
@@ -1,13 +1,12 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef SINGLE_MEMORY_H
+#define SINGLE_MEMORY_H
 
 #include <stdint.h>
 
-void* get_memory(uint64_t size);
-void* calloc_memory(uint64_t nmemb, uint64_t size);
-void* realloc_memory(void* ptr, uint64_t size);
-void free_memory(void* ptr);
-void release_memory();
-void display_memory();
+void*	get_memory(uint64_t size);
+void*	calloc_memory(uint64_t nmemb, uint64_t size);
+void*	realloc_memory(void* ptr, uint64_t size);
+void	free_memory(void* ptr);
+void	display_memory();
 
 #endif
